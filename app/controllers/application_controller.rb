a67f15f3d2ed262a0af941c6b8076384096e335e
Ian Brennan <ian.brennan@icloud.com>
2015-02-28 16:49:54 -0700

    hookup app user auth up and running

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if session[:user].present?
+    if session[:user_id].present?
       User.find(session[:user_id])
     end
   end
